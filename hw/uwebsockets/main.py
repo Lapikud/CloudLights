14c598165eb3483e65afb4b78d57363efed9d01a
Pearu <peapun@ttu.ee>
2017-10-26 23:33:26 +0300

    Initial upload of already put-together hardware scripts

---
+++ hw/uwebsockets/main.py
@@ -0,0 +1,54 @@
+"""Main is in the game."""
+import network
+import sys
+import uwebsockets.client
+import uwebsockets.protocol
+from machine import Pin
+
+# Create station and access point interfaces.
+sta_if = network.WLAN(network.STA_IF)
+ap_if = network.WLAN(network.AP_IF)
+
+# Activate station interface and deactivate access point interface to create a device instead of a router.
+sta_if.active(True)
+ap_if.active(False)
+
+# Connect station interface with WiFi, wait until they are connected.
+sta_if.connect("Lapikud", "wifiparool")
+while not sta_if.isconnected(): pass
+
+# Create object to turn on and off the pinout.
+pin_to_relay = Pin(0, Pin.OUT)
+
+# Save the socket address to string.
+uri = "ws://iot.wut.ee:80/ws/lap_esimene_lamp"
+
+# Connect with websocket and greet the server.
+print("I shall try to acquire a channel of communication:\n", uri)
+light1_web_socket = uwebsockets.client.connect(uri)
+light1_web_socket.send("I am foremost pleased to meet you.")
+
+# Keep updating the lamp state forever.
+while True:
+    try:
+        # Receive data from server websocket.
+        data = light1_web_socket.recv()
+    except OSError: # Connection timeout or reset
+        sys.exit() # Soft reset
+
+    # Update the pinout.
+    # Turn light on.
+    if data == "1":
+        pin_to_relay.on()
+
+    # Turn light off.
+    elif data == "0":
+        pin_to_relay.off()
+
+    # Send state to websocket.
+    elif data == "?":
+        light1_web_socket.send(str(pin_to_relay.value()))
+
+    # Do nothing.
+    else:
+        pass
